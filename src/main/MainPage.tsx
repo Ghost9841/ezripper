7269dea15c2ab6489e4d68b5009009dbd2aef406
Ghost9841 <asubedi778@gmail.com>
2025-07-26 18:02:55 +0545

    Fixed some Routing

--- src/main/MainPage.tsx
+++ src/main/MainPage.tsx
@@ -1,5 +1,7 @@
+import { useNavigate } from "react-router-dom";
 import HowItWorks from "./HowITWorks";
 const LandingPage = () => {
+  const navigate = useNavigate();
 
   return (
     <div className="">
@@ -13,18 +15,18 @@ const LandingPage = () => {
             Free online tools for GIF creation, video conversion, and image editing. No watermarks, no registration.
           </p>
           <div className="flex flex-col sm:flex-row justify-center gap-4">
-            <a
-              href="/video-tools/video-to-mp3"
+            <button
+              onClick={()=>  navigate('/video-tools/video-to-mp3')}
               className="bg-[#4c2278] hover:bg-[#5d2d8f] text-white font-bold py-3 px-8 rounded-lg transition transform hover:scale-105 shadow-lg"
             >
               Start Creating
-            </a>
-            <a
-              href="/coming-soon"
+            </button>
+            <button
+              onClick={()=> navigate("/coming-soon")}
               className="border-2 border-[#4c2278] text-[#4c2278] hover:bg-[#4c2278]/10 font-bold py-3 px-8 rounded-lg transition"
             >
               Explore Tools
-            </a>
+            </button>
           </div>
         </div>
       </section>
